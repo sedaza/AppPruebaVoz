e760ceb2479fa3043fcf0bd7a2ce8ed7c83e3fee
sedaza <sedaza@uan.edu.co>
2017-07-25 10:59:05 -0500

    visualizacion del espetro del audio y piano

---
+++ app/src/main/java/com/amcaicedo/sena/apppruebavoz/MainResultadoGlissando.java
@@ -0,0 +1,14 @@
+package com.amcaicedo.sena.apppruebavoz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainResultadoGlissando extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_resultado_glissando);
+
+    }
+}
